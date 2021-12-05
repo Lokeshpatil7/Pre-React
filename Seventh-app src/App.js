c42ab996649e3eb3a12a1b0e0e59d9dcec96592c
Lokeshpatil7 <92512124+Lokeshpatil7@users.noreply.github.com>
2021-12-05 17:42:44 +0530

    Add files via upload
    React-redux form creation submit/delete

---
+++ Seventh-app src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Person from './Person';
+import Course from './Course';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+     <h1>from app</h1>
+     {/* <Person/> */}
+     <Course/>
+    </div>
+    </>
+  );
+}
+
+export default App;
