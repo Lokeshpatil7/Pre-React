58b5df16dcab1cdcf34181031ddf552cb3819533
Lokeshpatil7 <92512124+Lokeshpatil7@users.noreply.github.com>
2021-12-08 17:18:37 +0530

    Add files via upload
    function/class component hooks

--- Seventh-app src/App.js
+++ Seventh-app src/App.js
@@ -2,14 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 import Person from './Person';
 import Course from './Course';
+import ClassComponentConversion from './ClassComponentConversion';
+import FunctionComponent from './FunctionComponent';
+import OnlineOrdersChild from './OnlineOrdersChild';
+import OnlineOrders from './OnlineOrders';
+import UseEffectExample from './UseEffectExample';
+
 
 function App() {
   return (
     <>
     <div className="App">
      <h1>from app</h1>
+     <UseEffectExample/>
+     {/* <OnlineOrders/>
+     <OnlineOrdersChild/> */}
+     {/* <ClassComponentConversion/> */}
+     {/* <FunctionComponent/> */}
      {/* <Person/> */}
-     <Course/>
+     {/* <Course/> */}
     </div>
     </>
   );
