d79dca28727b3df766ed4df57a8047a1e15e3c8b
Lokeshpatil7 <92512124+Lokeshpatil7@users.noreply.github.com>
2022-02-14 12:39:24 +0530

    Add files via upload

---
+++ tutorial app src/App.js
@@ -0,0 +1,25 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Tutorial from "./Tutorial";
+import Tutorials from "./Tutorials";
+import User from "./User";
+import Demo from "./Demo";
+import HooksSample from "./HooksSample";
+import Ui from "./Ui";
+import TutorialCrudUi from "./TutorialCrudUi";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Tutorial /> */}
+      {/* <Tutorials /> */}
+      {/* <User /> */}
+      <TutorialCrudUi />
+      {/* <HooksSample /> */}
+      {/* <Ui /> */}
+      {/* <Demo /> */}
+    </div>
+  );
+}
+
+export default App;
